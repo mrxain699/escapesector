15677299d3fd73a680dbfc91aedb55870a64f005
mrxain699 <zaininam699@gmail.com>
2023-11-01 20:33:20 -0700

    Update Commit

--- server.js
+++ server.js
@@ -14,25 +14,20 @@ const __dirname = dirname(__filename);
 const PORT = process.env.PORT;
 const DATABASE_URI = process.env.DATABASE_URI;
 
-app.use(
-  cors({
-    origin:
-      "['https://escape-sector-m6z6w7fpf-zayns-projects-4ca4d27b.vercel.app',]",
-  })
-);
+app.use(cors());
 app.use(
   express.json({
     limit: "100mb",
   })
 );
-app.use(express.static("static"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 
 connect(DATABASE_URI);
 
 app.use("/api", router);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/dist/index.html"));
+  res.sendFile(path.join(__dirname, "static/index.html"));
 });
 
 app.listen(PORT, () => {
